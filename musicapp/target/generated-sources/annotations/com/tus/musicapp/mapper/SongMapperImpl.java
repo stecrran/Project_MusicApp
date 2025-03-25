fae33554375934fc4196ac0b83321463d98477d7
Stephen C <A00325351@student.tus.ie>
2025-03-25 22:17:42 +0000

    updating project files, updating logging

--- musicapp/target/generated-sources/annotations/com/tus/musicapp/mapper/SongMapperImpl.java
+++ musicapp/target/generated-sources/annotations/com/tus/musicapp/mapper/SongMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-21T03:27:07+0000",
+    date = "2025-03-25T01:13:07+0000",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
