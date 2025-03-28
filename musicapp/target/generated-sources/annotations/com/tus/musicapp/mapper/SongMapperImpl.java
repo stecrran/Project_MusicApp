afa38f654e315b9530fdb67fcecb1aaf707d3db1
Stephen C <A00325351@student.tus.ie>
2025-03-28 19:16:33 +0000

    updating application properties for Jenkins

--- musicapp/target/generated-sources/annotations/com/tus/musicapp/mapper/SongMapperImpl.java
+++ musicapp/target/generated-sources/annotations/com/tus/musicapp/mapper/SongMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-28T03:32:15+0000",
+    date = "2025-03-28T19:09:58+0000",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.39.0.v20240820-0604, environment: Java 23.0.1 (Oracle Corporation)"
 )
 @Component
